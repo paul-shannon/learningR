3b01ea43af5d42deeab74f2a3e00b1a19dba44f2
thardy615 <thardy1@scmail.spelman.edu>
2019-07-30 12:00:01 -0700

    chapter 7 notes so far

---
+++ chapter.7.tanjanay.R
@@ -0,0 +1,20 @@
+##Chapter 7 Notes##
+
+String
+#element of a character vector, indictated by quotations
+c() 
+#creates character vectors
+paste(c()) 
+#combines strings together
+paste0(c())
+#same as paste(c()) but has no separator
+Sep= "symbol" 
+#separates the strings with a symbol or space, depending on what you type.
+toString() 
+#a variation of paste that is useful for printing vectors
+Width= # 
+#limits the output to an integer
+Cat 
+#works similar to paste but has less formatting. It can accept a file argument to write its output to a file.
+noquote()
+#suppresses quotes
